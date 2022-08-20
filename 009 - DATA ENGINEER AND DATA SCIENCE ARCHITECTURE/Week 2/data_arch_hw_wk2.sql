6c9ccf48d0d44a16beb7fc52815df9c9c192bdcb
Thaweewat <thaweewat.data@gmail.com>
2022-08-20 14:44:28 +0700

    Combined full_name string into one column

--- 009 - DATA ENGINEER AND DATA SCIENCE ARCHITECTURE/Week 2/data_arch_hw_wk2.sql
+++ 009 - DATA ENGINEER AND DATA SCIENCE ARCHITECTURE/Week 2/data_arch_hw_wk2.sql
@@ -3,25 +3,25 @@
 -- Retrieve the names of all employees in department 5 who work 
 -- more than 10 hours per week on the ProductX project.
 
-SELECT em.Fname AS 'full_name'
-     , wo.Hours AS 'work_hour'
-     , pj.Pname AS 'project_name'
-     , em.Dno   AS 'department_no'
+SELECT em.Fname + ' ' + em.Lname AS 'employee_name'
+     , wo.Hours                  AS 'work_hour'
+     , pj.Pname                  AS 'project_name'
+     , em.Dno                    AS 'department_no'
 FROM dbo.EMPLOYEE em,
      dbo.PROJECT pj,
      dbo.WORKS_ON wo
-WHERE Essn = em.Ssn
-  AND Dno = '5'
+WHERE wo.Essn = em.Ssn
+  AND em.Dno = '5'
   AND Hours > 10
-  AND Pname = 'ProductX'
+  AND pj.Pname = 'ProductX'
 ;
 
 -- =============================================
 -- Exam 2
 -- List top 2 employee (ssn) who work the most hours across the projects
 
-SELECT TOP 2 em.Fname AS 'full_name'
-           , wo.Hours AS 'work_hour'
+SELECT TOP 2 em.Fname + ' ' + em.Lname AS 'employee_name'
+           , wo.Hours                  AS 'work_hour'
 FROM dbo.EMPLOYEE em,
      dbo.WORKS_ON wo
 WHERE wo.Essn = em.Ssn
@@ -32,7 +32,7 @@ ORDER BY Hours DESC
 -- Exam 3
 -- Find the names of all employees who are directly supervised by ‘Franklin Wong’.
 
-SELECT em.Fname AS 'full_name'
+SELECT em.Fname + ' ' + em.Lname AS 'employee_name'
 FROM dbo.EMPLOYEE em
 WHERE Super_ssn = (SELECT Ssn
                    FROM dbo.EMPLOYEE
@@ -44,9 +44,8 @@ WHERE Super_ssn = (SELECT Ssn
 -- Exam 4
 -- Who is the manager of Research Department?
 
-SELECT em.Fname AS 'full_name',
-       em.Lname AS 'last_name',
-       dp.Dname AS 'department_name'
+SELECT em.Fname + ' ' + em.Lname AS 'employee_name'
+     , dp.Dname                  AS 'department_name'
 FROM dbo.EMPLOYEE em,
      dbo.DEPARTMENT dp
 WHERE em.Dno = dp.Dnumber
@@ -57,14 +56,12 @@ WHERE em.Dno = dp.Dnumber
 -- Exam 5
 -- List the names of all employees who have salary more than their supervisor.
 
-SELECT em.Fname   AS 'full_name'
-     , em.Lname   AS 'last_name'
-     , em2.Fname  AS 'supervisor_name'
-     , em.Salary  AS 'salary'
-     , em2.Salary AS 'supervisor_salary'
+SELECT em.Fname + ' ' + em.Lname   AS 'employee_name'
+     , em.Salary                   AS 'employee_salary'
+     , em2.Fname + ' ' + em2.Lname AS 'supervisor_name'
+     , em2.Salary                  AS 'supervisor_salary'
 FROM dbo.EMPLOYEE em,
      dbo.EMPLOYEE em2
 WHERE em.Super_Ssn = em2.ssn
   AND em.SALARY > em2.SALARY
-  AND em.Super_ssn IS NOT NULL
 ;
