c1f710fa165842402d440422d1fca8237be92297
Thaweewat <thaweewat.data@gmail.com>
2022-08-20 12:31:38 +0700

    Uploaded HW and Ref

---
+++ 009 - DATA ENGINEER AND DATA SCIENCE ARCHITECTURE/Week 2/data_arch_hw_wk2.sql
@@ -0,0 +1,64 @@
+-- =============================================
+-- Exam 1
+-- Retrieve the names of all employees in department 5 who work 
+-- more than 10 hours per week on the ProductX project.
+
+SELECT DISTINCT(em.Fname) AS 'full_name'
+              , wo.Hours  AS 'work_hour'
+              , pj.Pname  AS 'project_name'
+              , em.Dno    AS 'department_no'
+FROM dbo.EMPLOYEE em,
+     dbo.PROJECT pj,
+     dbo.WORKS_ON wo
+WHERE Dno = '5'
+  AND Hours > 10
+  AND Pname = 'ProductX';
+
+-- =============================================
+-- Exam 2
+-- List top 2 employee (ssn) who work the most hours 
+-- across the projects
+
+SELECT TOP 2 em.Fname AS 'full_name'
+           , wo.Hours AS 'work_hour'
+FROM dbo.EMPLOYEE em,
+     dbo.WORKS_ON wo
+ORDER BY Hours DESC;
+
+-- =============================================
+-- Exam 3
+-- Find the names of all employees who are directly
+-- supervised by ‘Franklin Wong’.
+
+SELECT em.Fname AS 'full_name'
+FROM dbo.EMPLOYEE em
+WHERE Super_ssn = (SELECT Ssn
+                   FROM dbo.EMPLOYEE
+                   WHERE Fname like 'Franklin'
+                     AND Lname like 'Wong');
+
+-- =============================================
+-- Exam 4
+-- Who is the manager of Research Department?
+
+SELECT em.Fname AS 'full_name'
+FROM dbo.EMPLOYEE em,
+     dbo.DEPARTMENT
+WHERE Super_ssn IS NULL
+  AND Dname = 'Research';
+
+-- =============================================
+-- Exam 5
+-- List the names of all employees who have salary more
+-- than their supervisor.
+
+SELECT em.Fname   AS 'full_name'
+     , em.Lname   AS 'last_name'
+     , em2.Fname  AS 'superviser_name'
+     , em.Salary  AS 'salary'
+	 , em2.Salary AS 'superviser_salary'
+FROM dbo.EMPLOYEE em,
+     dbo.EMPLOYEE em2
+WHERE em.Super_Ssn = em2.ssn
+  AND em.SALARY > em2.SALARY
+  AND em.Super_ssn IS NOT NULL;
